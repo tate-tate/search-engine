5cd8f4213fd979b48860afb7c42af5a093af61fe
Tate Sever <gabrieltsever@gmail.com>
2025-04-27 14:57:33 -0400

    trying to fix pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-base: '/',
+  base: '/search-engine/', // Set the base path to match the repository name
+  plugins: [react()],
 });
